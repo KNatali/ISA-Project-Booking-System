105ab1e48ec6d7f99bee62bf90bb6fc69d00fa0a
aleksandra.b <aleksandra.borisavljevic99@gmail.com>
2021-11-13 16:33:19 +0100

    added data.sql file

---
+++ ISA-project/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+use isa;
+insert into navigation_equipment(name) values('bilo sta');
+insert into adventure_report(average_grade,revenue) values (7,1900);
+insert into boat_report(average_grade,revenue) values (4,600);
