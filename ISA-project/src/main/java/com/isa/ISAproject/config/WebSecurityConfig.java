935c24f80aaf54f4807a49c3a55060547204e606
aleksandra.b <aleksandra.borisavljevic99@gmail.com>
2021-12-10 17:54:57 +0100

    added endpoint in config

--- ISA-project/src/main/java/com/isa/ISAproject/config/WebSecurityConfig.java
+++ ISA-project/src/main/java/com/isa/ISAproject/config/WebSecurityConfig.java
@@ -91,6 +91,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 									.antMatchers("/auth/signup").permitAll()
 									.antMatchers("/confirm-registration-client/**").permitAll()
 									.antMatchers("/api/additional-items/**").permitAll()
+									.antMatchers("/api/instructors").permitAll()
 				// ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
 				// koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
 				// samo korisnik koji ima rolu 'ADMIN', navodimo na sledeci nacin: 
