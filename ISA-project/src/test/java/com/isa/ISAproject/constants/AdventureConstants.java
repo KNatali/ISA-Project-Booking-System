73ffd318d2edbc946ad9c00d1aa44301e871ef93
KNatali <krsmanovic.in21.2018@uns.ac.rs>
2022-01-10 17:06:42 +0100

    done unit tests

--- ISA-project/src/test/java/com/isa/ISAproject/constants/AdventureConstants.java
+++ ISA-project/src/test/java/com/isa/ISAproject/constants/AdventureConstants.java
@@ -2,16 +2,28 @@ package com.isa.ISAproject.constants;
 
 public class AdventureConstants {
 	public static final String NEW_NAME = "Fun fishing";
-    public static final String NEW_STREET = "3151 West 10th Street";
-    public static final String NEW_CITY = "Panama City";
-    public static final String NEW_STATE = "United States";
+	public static final String NEW_DESCRIPTION = "Have fun with us";
+	public static final double NEW_GRADE = 8;
+	public static final double NEW_PRICE = 250;
+	public static final int NEW_PERSONS = 5;
+	public static final int NEW_PERCENTAGE = 10;
+    public static final String NEW_ADDRESS_STREET = "3151 West 10th Street";
+    public static final String NEW_ADDRESS_CITY = "Panama City";
+    public static final String NEW_ADDRESS_STATE = "United States";
     public static final Long NEW_ID = 4L;
     
+    public static final String DB_NAME = "Star fish";
+	public static final String DB_DESCRIPTION = "Best adventure";
+	public static final double DB_GRADE = 9;
+	public static final double DB_PRICE = 300;
+	public static final int DB_PERSONS = 7;
+	public static final int DB_PERCENTAGE = 15;
+    public static final String DB_ADDRESS_STREET = "3100 West 12th Street";
+    public static final String DB_ADDRESS_CITY = "Miami";
+    public static final String DB_ADDRESS_STATE = "United States";
     public static final Long DB_ID = 1L;
-    public static final String DB_NAME = "Star boat";
-    public static final String DB_STREET = "3400 Pan American Dr";
-    public static final String DB_CITY = "Miami";
-    public static final String DB_STATE = "United States";
+    
+   
     
     //number of students whose last name is DB_LASTNAME_COUNT
     public static final String DB_NAME_COUNT = "Stević";
