9e102801bb5b194f0f23920a98962baef7239d60
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2022-01-20 17:50:31 +0100

    Add in data.sql

--- ISA-project/src/main/resources/data.sql
+++ ISA-project/src/main/resources/data.sql
@@ -208,7 +208,7 @@ insert into boat_fast_reservation(reservation_start,reservation_end,date,time,du
 insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2016-02-20T06:30:00",10,10,15000,5,1,'2021-11-10-07-00-00','2021-11-12-13-00-00',20);
 insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2014-02-20T06:30:00",3,2,7000,8,2,'2021-12-4-07-00-00','2021-12-20-14-00-00',30);
 insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2023-02-20T06:30:00",1,3,3000,8,3,'2021-11-10-07-00-00','2021-11-12-13-00-00',40);
-insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2017-02-20T06:30:00",3,1,6000,8,1,'2022-1-4-07-00-00','2022-1-20-14-00-00',50);
+insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2017-02-20T06:30:00",3,1,6000,8,1,'2022-1-4-07-00-00','2022-1-30-14-00-00',50);
 insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2022-02-20T06:30:00",4,5,9000,8,4,'2022-11-10-07-00-00','2022-11-12-13-00-00',60);
 insert into cottage_reservation(date,duration,max_persons,price,client_id,cottage_id,reservation_start,reservation_end,system_earning) values ("2021-02-20T06:30:00",2,5,3400,9,2,'2022-12-4-07-00-00','2022-12-20-14-00-00',10);
 
@@ -240,7 +240,7 @@ insert into client_cottage_reservations(client_id,cottage_reservations_id) value
 insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2016-02-20T06:30:00",10,10,15000,5,1,'2021-11-10-07-00-00','2021-11-12-13-00-00',120);
 insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2014-02-20T06:30:00",3,2,7000,8,2,'2021-12-4-07-00-00','2021-12-20-14-00-00', 150);
 insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2021-02-20T06:30:00",1,3,3000,8,3,'2021-11-10-07-00-00','2021-11-12-13-00-00', 200);
-insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2017-02-20T06:30:00",3,1,6000,8,1,'2022-1-4-07-00-00','2022-1-20-14-00-00', 250);
+insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2017-02-20T06:30:00",3,1,6000,8,1,'2022-1-4-07-00-00','2022-1-30-14-00-00', 250);
 insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2025-02-20T06:30:00",4,5,9000,8,4,'2022-11-10-07-00-00','2022-11-12-13-00-00', 300);
 insert into boat_reservation(date,duration,max_persons,price,client_id,boat_id,reservation_start,reservation_end,system_earning) values ("2021-02-20T06:30:00",2,5,3400,9,2,'2022-12-4-07-00-00','2022-12-20-14-00-00', 100);
 
